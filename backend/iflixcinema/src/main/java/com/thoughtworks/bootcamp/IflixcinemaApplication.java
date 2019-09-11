e4540e20663f210a53469f088f1f5790e8f7a957
sunilsyenare <syenare97@gmail.com>
2019-09-11 16:01:17 +0530

    [sunil] Add. initialize project

---
+++ backend/iflixcinema/src/main/java/com/thoughtworks/bootcamp/IflixcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IflixcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IflixcinemaApplication.class, args);
+	}
+
+}
